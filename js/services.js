b3b50e42e9bd50d5cdbe1c37e1a4e79d9f2176b9
Rootdiv <rootdiv@mail.ru>
2021-12-08 18:42:06 +0300

    feat: movie of the week get data from the server

---
+++ js/services.js
@@ -0,0 +1,17 @@
+'use strict';
+const API_KEY = 'fc4d5431d481c3e7e28e4c88d61a3581';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+const getData = url => fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw `Что-то пошло не так, ошибка ${response.status}`;
+  }).catch(err => console.error(err));
+
+export const getTrends = async (type = 'all', period = 'day', page = 1) => {
+  const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+  return await getData(url);
+};
